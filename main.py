a6373701505d611930bf2fedf648b0913a82ae63
cottongin <cottongin@users.noreply.github.com>
2020-09-11 04:36:24 -0500

    main: devbot logger output

--- main.py
+++ main.py
@@ -10,6 +10,11 @@ try:
     # dev build only requires this
     load_dotenv('.env')
     dev_bot = os.getenv("DEVBOT")
+    logger = logging.getLogger('discord')
+    logger.setLevel(logging.DEBUG)
+    handler = logging.FileHandler(filename='discord.log', encoding='utf-8', mode='w')
+    handler.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s:%(name)s: %(message)s'))
+    logger.addHandler(handler)
 except:
     # heroku stores this
     dev_bot = False
